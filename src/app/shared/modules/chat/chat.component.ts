8f7dbd5f770d4188ca5ea536ac9965fb90c235c0
renu sharma <renusharma.iet@gmail.com>
2018-03-27 11:31:03 +0530

    chat implemented

--- src/app/shared/modules/chat/chat.component.ts
+++ src/app/shared/modules/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { DataService } from '../../data' 
 declare var Ps: any;
 const id = "FB1Z7IJI01"
@@ -9,6 +9,7 @@ const id = "FB1Z7IJI01"
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
+	@Input() idvalue: string;
 	newMsg : string = "";
 	newName: string = "";
 	senderName :string = "";
@@ -34,13 +35,17 @@ export class ChatComponent implements OnInit {
     		if(this.newName == ""){
     			this.newName = "Customer";
     		}
-    		var newObj = {}
-    		newObj = {name:this.newName,msg:this.newMsg,from:this.senderName};
+    		var newObj = {};
+    		var currentdate = new Date();
+            var date = currentdate.getDate() + "/" + (currentdate.getMonth() + 1) + "/" + currentdate.getFullYear();
+            var time = currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+            var datetime = date + " at " + time;
+    		newObj = {name:this.newName,msg:this.newMsg,from:this.senderName,datetime:datetime};
 	        this.nameList.push(newObj);
 	        this.newName = '';
 	        this.newMsg = '';	
 	        this.senderName = '';       
-	        this.dataservice.post_msg(id,newObj).subscribe(post_data =>{
+	        this.dataservice.post_msg(this.idvalue,newObj).subscribe(post_data =>{
 	        	console.log(post_data);
 	        })   
 	        return true;     
@@ -54,7 +59,7 @@ export class ChatComponent implements OnInit {
     }
 
     get_msgList(){
-    	this.dataservice.get_msg(id).subscribe(response =>{
+    	this.dataservice.get_msg(this.idvalue).subscribe(response =>{
     		console.log(response);
     		if(response.data == "1"){
     			this.nameList = response["items"];
